df0e4af9dfffb7c5d77eaa465dfd8ee1aaffa98f
davemcg <mcgaughey@gmail.com>
2016-07-07 17:15:16 -0400

    case control plotting

---
+++ quick_plot.R
@@ -0,0 +1,11 @@
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+tBeta <- fread('Illumina_beta.SWAN.fixOutliers.t.IDs.dat')
+
+cg_positions <- DMPs %>% filter(adj.P.Val < 0.05) %>% .[['Name']]
+plot_data <- tBeta %>% select(one_of(c("Sample",cg_positions))) %>% left_join(.,metadata) %>% select(one_of(c("Sample","Case.Control",cg_positions))) %>% gather(key=Sample,Case.Control)
+colnames(plot_data)<-c("Sample","Case.Control","cg","Value")
+ggplot(data=plot_data,aes(x=Case.Control,y=Value)) + geom_jitter() + geom_boxplot(alpha=0.5) + ylim(c(0,1)) + facet_wrap(~cg)
