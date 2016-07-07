7cc8a6f6e626911adabab205b12c3ea9e630355f
davemcg <mcgaughey@gmail.com>
2016-07-07 16:58:48 -0400

    properly labelled plots, limma fitting set up, plots DMR, does variance analysis

--- maksimovic_flow.R
+++ maksimovic_flow.R
@@ -23,20 +23,18 @@ library(dplyr)
 ann450k <-  getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 
 # bring in raw data (Illumina .idat)
-setwd('/data/mcgaugheyd/projects/nhgri/brody/fetal_alcohol_methylation_study/')
-metadata <- fread('sentrix_id_conversion_table_demographics_v6.txt')
-targets <- read.metharray.sheet("/data/mcgaugheyd/projects/nhgri/brody/fetal_alcohol_methylation_study/Tsai_Sample_083",pattern="Sheet.csv$")
+metadata <- fread('~/git/fetal_alcohol/sentrix_id_conversion_table_demographics_v6.txt')
+targets <- read.metharray.sheet("/Volumes/ThunderBay/PROJECTS/brody/fetal_alcohol/Tsai_Sample_083/",pattern="Sheet.csv$")
 rgSet <- read.metharray.exp(targets=targets)
-sampleNames(rgSet) <- targets$Sample_Name
+sampleNames(rgSet) <- rgSet$Sample_Name
 
-# add sample group info to targets
-full_sample_names <- targets$Sample_Name
+# add sample group info to targets, fewer rows in metadata, already parsed down to remove outliers
 targets <- left_join(data.frame(metadata),targets,by=c("Sample"="Sample_Name"))
 targets$Sample_Group <- targets$Case.Control
 targets$Sample_Name <- targets$Sample
 
 # remove samples based on previous outlier analysis / ethnicity
-keep <-  full_sample_names %in% targets$Sample_Name
+keep <-  rgSet$Sample_Name %in% targets$Sample_Name
 rgSet <- rgSet[,keep]
 
 # calculate the detection p-values
@@ -45,6 +43,9 @@ head(detP)
 
 # plot detection p-values
 # everything looks fine in this data-set
+# first match up targets to detP
+targets <- targets[match(colnames(detP), targets$Sample),]
+
 pal <- brewer.pal(8,"Dark2")
 par(mfrow=c(1,2))
 barplot(colMeans(detP), col=pal[factor(targets$Sample_Group)], las=2,
@@ -66,6 +67,7 @@ mSetSq <- preprocessQuantile(rgSet)
 # create a MethylSet object from the raw data for plotting
 mSetRaw <- preprocessRaw(rgSet)
 # visualise what the data looks like before and after normalization
+targets <- targets[match(mSetSq$Sample_Name, targets$Sample),]
 par(mfrow=c(1,2))
 densityPlot(rgSet, sampGroups=targets$Sample_Group,main="Raw", legend=FALSE)
 densityPlot(getBeta(mSetSq), sampGroups=targets$Sample_Group,
@@ -73,6 +75,9 @@ densityPlot(getBeta(mSetSq), sampGroups=targets$Sample_Group,
 
 
 # MDS plots to look at largest sources of variation
+# first, line up targets and mSetSq
+targets <- targets[match(mSetSq$Sample_Name, targets$Sample),]
+
 par(mfrow=c(2,2))
 plotMDS(getM(mSetSq), top=1000, gene.selection="common",
         col=pal[factor(targets$Sample_Group)])
@@ -103,16 +108,15 @@ mSetSqFlt <- mSetSq[keep,]
 mSetSqFlt
 
 # if your data includes males and females, remove probes on the sex chromosomes
-keep <- !(featureNames(mSetSqFlt) %in% ann450k$Name[ann450k$chr %in%
-                                                      c("chrX","chrY")])
+keep <- !(featureNames(mSetSqFlt) %in% ann450k$Name[ann450k$chr %in%  c("chrX","chrY")])
 mSetSqFlt <- mSetSqFlt[keep,]
 table(keep)
 
 # pull in my cg sites that I'm skipping due to overlap with SNPs and 
 # are cross-reactive and are on chrs X Y
-cg_remove_1 <- scan('450k_cg_sites_that_LITERALLY_are_on_SNPS.maf005_somepop_plus1bp.txt',what='character')
-cg_remove_2 <- scan('450k_cg_sites_that_overlap_dbsnp138.maf005_somepop_plus1bp.txt',what='character')
-xReactive <- scan('illumina450k_positions_to_exclude.not_including_dbsnp_overlapping.withChen.dat',what='character')
+cg_remove_1 <- scan('~/git/fetal_alcohol/450k_cg_sites_that_LITERALLY_are_on_SNPS.maf005_somepop_plus1bp.txt',what='character')
+cg_remove_2 <- scan('~/git/fetal_alcohol/450k_cg_sites_that_overlap_dbsnp138.maf005_somepop_plus1bp.txt',what='character')
+xReactive <- scan('~/git/fetal_alcohol/illumina450k_positions_to_exclude.not_including_dbsnp_overlapping.withChen.dat',what='character')
 cg_remove <- c(cg_remove_1, cg_remove_2, xReactive)
 cg_remove <- unique(cg_remove)
 
@@ -123,6 +127,8 @@ mSetSqFlt <- mSetSqFlt[keep,]
 
 # MDS plots to look at largest sources of variation
 # now with the filtered set
+# again, ensure targets and mSetSqFlt line up
+targets <- targets[match(mSetSqFlt$Sample_Name, targets$Sample),]
 par(mfrow=c(2,2))
 plotMDS(getM(mSetSqFlt), top=1000, gene.selection="common",
         col=pal[factor(targets$Sample_Group)])
@@ -147,10 +153,10 @@ legend("top", legend=levels(factor(targets$Sample_Plate)), text.col=pal,
 
 # calculate M-values for statistical analysis
 mVals <- getM(mSetSqFlt)
-head(mVals[,1:5])
+#head(mVals[,1:5])
 
 bVals <- getBeta(mSetSqFlt)
-head(bVals[,1:5])
+#head(bVals[,1:5])
 
 par(mfrow=c(1,2))
 densityPlot(bVals, sampGroups=targets$Sample_Group, main="Beta values",
@@ -158,7 +164,8 @@ densityPlot(bVals, sampGroups=targets$Sample_Group, main="Beta values",
 densityPlot(mVals, sampGroups=targets$Sample_Group, main="M-values",
             legend=FALSE, xlab="M values")
 
-
+# explicitly match targets sample order (rows) with mVars
+targets <- targets[match(colnames(mVals), targets$Sample),]
 
 
 case_control <- factor(targets$Case.Control)
@@ -172,10 +179,39 @@ mono <- as.numeric(targets$Mono)
 gran <- as.numeric(targets$Gran)
 smoking <- factor(targets$Smoking)
 
-design <- model.matrix(~0+case_control+Smoking+gender+cd8t+cd4t+nk+mono+gran+ethnicity, data=targets)
-colnames(design)<-c("Case","Control","Smoking","Gender","CD8T","CD4","NK","MONO","GRAN","Hispanic","Other","White")
+design <- model.matrix(~0+case_control+gender+cd8t+cd4t+nk+mono+gran+ethnicity, data=targets)
+colnames(design)<-c("Case","Control","Gender","CD8T","CD4","NK","MONO","GRAN","Hispanic","Other","White")
 cmtx <- makeContrasts( "Case-Control", levels=design)
 fit <- lmFit(mVals, design)
 fit2 <- contrasts.fit(fit, cmtx)
 fit2 <- eBayes(fit2)
 summary(decideTests(fit2))
+
+
+# get the table of results for the first contrast 
+ann450kSub <- ann450k[match(rownames(mVals),ann450k$Name),
+                      c(1:4,12:19,24:ncol(ann450k))]
+DMPs <- topTable(fit2, num=Inf, coef=1, genelist=ann450kSub)
+head(DMPs)
+
+
+
+myAnnotation <- cpg.annotate(mVals, datatype = "array",
+                             analysis.type="differential", design=design,
+                             contrasts = TRUE, cont.matrix = cmtx,
+                             coef="Case-Control")
+
+DMRs <- dmrcate(myAnnotation, lambda=1000, C=2)
+# grab ranges for plotting
+results.ranges <- extractRanges(DMRs, genome = "hg19")
+groups <- c(Case='magenta',Control='forestgreen')
+cols <- groups[as.character(targets$Case.Control)]
+DMR.plot(ranges=results.ranges,dmr=1,CpGs=mVals,phen.col=as.factor(targets$Case.Control),genome='hg19')
+DMR.plot(ranges=results.ranges,dmr=1,CpGs=mVals,phen.col=cols,genome='hg19',samps=c(1:10))
+
+
+
+# variance analysis
+design <- model.matrix(~factor(targets$Sample_Group))
+fitvar <- varFit(mVals, design = design, coef = c(1,2))
+topDV <- topVar(fitvar, coef=2)
