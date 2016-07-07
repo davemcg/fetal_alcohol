62542ca79b13317a566d476758b59562979010ed
davemcg <mcgaughey@gmail.com>
2016-07-07 17:23:12 -0400

    individual plot added

--- quick_plot.R
+++ quick_plot.R
@@ -9,3 +9,8 @@ cg_positions <- DMPs %>% filter(adj.P.Val < 0.05) %>% .[['Name']]
 plot_data <- tBeta %>% select(one_of(c("Sample",cg_positions))) %>% left_join(.,metadata) %>% select(one_of(c("Sample","Case.Control",cg_positions))) %>% gather(key=Sample,Case.Control)
 colnames(plot_data)<-c("Sample","Case.Control","cg","Value")
 ggplot(data=plot_data,aes(x=Case.Control,y=Value)) + geom_jitter() + geom_boxplot(alpha=0.5) + ylim(c(0,1)) + facet_wrap(~cg)
+
+
+# individual plot
+ggplot(data=subset(plot_data,cg=='cg13220109'),aes(x=Case.Control,y=Value)) + geom_jitter() + geom_boxplot(alpha=0.5) + ylim(c(0,1))
+       
